fa7e9f8e0d581136908494950b9d9a091e972f37
jps27CSE <jackpritomsoren@hotmail.com>
2023-10-16 21:31:52 +0600

    Sign In Layout

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
+import LinkedinLogo from "../assets/linkedinLogo.png";
 import "../Sass/LoginComponent.scss";
 import { LoginAPI } from "../api/AuthAPI";
+import { useNavigate } from "react-router-dom";
 const LoginComponent = () => {
-  const [credentials, setCredentials] = useState({});
+  const [credentails, setCredentials] = useState({});
+  let navigate = useNavigate();
   const login = async () => {
     try {
-      let res = await LoginAPI(credentials.email, credentials.password);
+      let res = await LoginAPI(credentails.email, credentails.password);
       console.log(res);
     } catch (error) {
       console.log(error);
@@ -13,30 +16,44 @@ const LoginComponent = () => {
   };
 
   return (
-    <div>
-      <h1>Login Component</h1>
-      <div className="auth-inputs">
-        <input
-          type="email"
-          onChange={(event) =>
-            setCredentials({ ...credentials, email: event.target.value })
-          }
-          className="common-input"
-          placeholder="Enter your Email"
-        />
+    <div className="login-wrapper">
+      <img src={LinkedinLogo} className="linkedinLogo" />
 
-        <input
-          type="email"
-          onChange={(event) =>
-            setCredentials({ ...credentials, password: event.target.value })
-          }
-          className="common-input"
-          placeholder="Enter your Password"
-        />
+      <div className="login-wrapper-inner">
+        <h1 className="heading">Sign in</h1>
+        <p className="sub-heading">Stay updated on your professional world</p>
+
+        <div className="auth-inputs">
+          <input
+            onChange={(event) =>
+              setCredentials({ ...credentails, email: event.target.value })
+            }
+            type="email"
+            className="common-input"
+            placeholder="Email or Phone"
+          />
+          <input
+            onChange={(event) =>
+              setCredentials({ ...credentails, password: event.target.value })
+            }
+            type="password"
+            className="common-input"
+            placeholder="Password"
+          />
+        </div>
+        <button onClick={login} className="login-btn">
+          Sign in
+        </button>
       </div>
-      <button onClick={login} className="login-btn">
-        Login
-      </button>
+      <hr className="hr-text" data-content="or" />
+      <div className="google-btn-container">
+        <p className="go-to-signup">
+          New to LinkedIn?{" "}
+          <span className="join-now" onClick={() => navigate("/register")}>
+            Join now
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./index.scss";
 import { router } from "./Routes/index.jsx";
 import { RouterProvider } from "react-router-dom";
 import { app } from "./Firebase/FirebaseConfig";
