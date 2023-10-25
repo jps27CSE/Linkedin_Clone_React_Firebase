6386809f1d6cc0325e71bf5b68d00ee917361bfe
jps27CSE <jackpritomsoren@hotmail.com>
2023-10-25 12:44:52 +0600

    Creating an Auth Guard

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
 import HomeComponent from "../components/HomeComponent";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../Firebase/FirebaseConfig";
+import { useNavigate } from "react-router-dom";
+import Loader from "../components/common/Loader";
 
 const Home = () => {
-  return (
-    <div>
-      <HomeComponent />
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  useEffect(() => {
+    onAuthStateChanged(auth, (res) => {
+      if (!res?.accessToken) {
+        navigate("/");
+      } else {
+        setLoading(false);
+      }
+    });
+  }, [navigate]);
+
+  return <div>{loading ? <Loader /> : <HomeComponent />}</div>;
 };
 
 export default Home;
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import LoginComponent from "../components/LoginComponent";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../Firebase/FirebaseConfig";
+import { useNavigate } from "react-router-dom";
+import Loader from "../components/common/Loader";
 
 const Login = () => {
-  return <LoginComponent />;
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  useEffect(() => {
+    onAuthStateChanged(auth, (res) => {
+      if (res?.accessToken) {
+        navigate("/home");
+      } else {
+        setLoading(false);
+      }
+    });
+  }, [navigate]);
+
+  return loading ? <Loader /> : <LoginComponent />;
 };
 
 export default Login;
